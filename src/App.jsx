e3907d119c86816835b1d37d02937ac1d1e72fbe
ankitha <yankitha65@gmail.com>
2025-03-12 15:31:27 +0530

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import StopWatch from './StopWatch.jsx'
 function App() {
        
         const fruits = [{id:1, name: "apple", calories: 95},
-          {id:2, name: "orange", calories: 45},
+          {id:2, name: "myfruit", calories: 45},
           {id:3, name: "banana", calories: 105},
           {id:4, name: "coconut", calories: 159},
           {id:5, name: "pineapple", calories: 37}
